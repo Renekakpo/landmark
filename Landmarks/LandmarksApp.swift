1c5822ef1229e18f1ac7e6f0d97ab67d39ce29dc
rene <rene@rightcom.com>
2023-11-07 12:48:21 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by RightMac-Rene on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
